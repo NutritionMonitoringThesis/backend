767cc01126215637eb11059bfb3a91b72937b121
Avei20 <muhammadabdulazizalghofari@gmail.com>
2023-07-09 23:16:43 +0700

    Adding Standard Gizi for Children

--- src/controller/standardGiziController.ts
+++ src/controller/standardGiziController.ts
@@ -1,6 +1,7 @@
 import { Prisma, PrismaClient } from "@prisma/client"
 import { Request, Response } from "express"
 import { getId } from "./authController"
+import moment from "moment"
 
 const prisma = new PrismaClient()
 
@@ -8,21 +9,101 @@ const prisma = new PrismaClient()
 export const getStandardGizi = async (req:Request, res:Response) => {
     const token = req.headers['auth'] as string
     const userId = getId(token)
+    const data = req.query
 
-    prisma.userDetails.findUnique({
-        where : {
-            userId: userId
+    if (data.anakId === null) {
+
+        await prisma.userDetails.findUnique({
+            where : {
+                userId: userId
+            }
+        })
+        .then(userDetails => {
+            const tanggalLahir = userDetails?.tanggalLahir
+            const tahunLahir = tanggalLahir?.getFullYear() as number
+            const tanggalNow = new Date
+            const tahunNow = tanggalNow.getFullYear()
+            const umur = tahunNow - tahunLahir
+            console.log(tahunLahir + '-' + tahunNow)
+            console.log(umur)
+            const gender = userDetails?.jenisKelamin
+            let kelompok 
+            if (gender == 'M' ) {
+                kelompok = 'Laki-Laki'
+            }
+            else if (gender == 'F') {
+                kelompok = 'Perempuan'
+            }
+    
+            prisma.standarGizi.findFirst({
+                where : {
+                    akhirRentang : { gte : umur},
+                    awalRentang : { lte : umur},
+                    kelompok : kelompok,
+    
+                },
+                include : {
+                    standarGiziDetail : true
+                }
+            })
+            .then (data => {
+                // console.log(data)
+                res.send({
+                    success: true, 
+                    message: 'Ini yah data standar stuntingnya, semoga anak anda tidak stunting hehe.',
+                    data: data
+                })
+            })
+            .catch (err => {
+                console.log(err)
+                if (err instanceof Prisma.PrismaClientKnownRequestError) {
+                    if (err.code == 'P2015') {
+                        res.status(404).send({ 
+                            success: false,
+                            message: 'Not Found!'
+                        })
+                        return
+                    }
+                }
+                res.status(500).send({
+                    success: false,
+                    message: 'Error Occured Contact Admin'
+                })
+                return 
+                
+            })
+        })
+    }
+    else {
+        // Query Standard Anak 
+        const { anakId } = req.query
+
+        // Check Anak Data 
+        const anak = await prisma.anak.findFirstOrThrow({
+            where: {
+                id: anakId as string,
+                parentId: userId,
+            }
+        })
+
+        if (anak === null) {
+            res.status(404).send({
+                success: false, 
+                message: 'Data anak tidak ditemukan'
+            })
+            return 
         }
-    })
-    .then(userDetails => {
-        const tanggalLahir = userDetails?.tanggalLahir
-        const tahunLahir = tanggalLahir?.getFullYear() as number
-        const tanggalNow = new Date
-        const tahunNow = tanggalNow.getFullYear()
-        const umur = tahunNow - tahunLahir
-        console.log(tahunLahir + '-' + tahunNow)
-        console.log(umur)
-        const gender = userDetails?.jenisKelamin
+
+        // Calculate Umur to Bulan and Tahun
+
+        // If tahun < 0 konversi ke bulan 
+        const tanggalLahir = moment(anak.tanggalLahir)
+        const today = moment()
+        const year = today.diff(tanggalLahir, 'years')
+        const month = today.diff(tanggalLahir, 'months')
+
+        // Check gender yagesya  
+        const gender = anak.jenisKelamin
         let kelompok 
         if (gender == 'M' ) {
             kelompok = 'Laki-Laki'
@@ -30,24 +111,39 @@ export const getStandardGizi = async (req:Request, res:Response) => {
         else if (gender == 'F') {
             kelompok = 'Perempuan'
         }
+        
+        let umur 
+        let satuan 
 
-        prisma.standarGizi.findFirst({
-            where : {
-                akhirRentang : { gte : umur},
-                awalRentang : { lte : umur},
-                kelompok : kelompok,
+        if ( year < 1 ) {
+            umur = month
+            satuan = 'bulan'
+        }
+        else {
+            umur = year
+            satuan = 'tahun'
+        }
 
+        await prisma.standarGizi.findFirst({
+            where: {
+                satuan: satuan,
+                akhirRentang: {
+                    gte: umur
+                },
+                awalRentang: {
+                    lte: umur
+                },
+                kelompok: kelompok
             },
-            include : {
-                standarGiziDetail : true
+            include: {
+                standarGiziDetail: true,
             }
         })
-        .then (data => {
-            // console.log(data)
+        .then(data => {
             res.send({
-                success: true, 
-                message: 'Ini yah data standar stuntingnya, semoga anak anda tidak stunting hehe.',
-                data: data
+                success: true,
+                message: 'Ini yah standard stunting anaknya',
+                data: data,
             })
         })
         .catch (err => {
@@ -61,11 +157,13 @@ export const getStandardGizi = async (req:Request, res:Response) => {
                     return
                 }
             }
+            
             res.status(500).send({
                 success: false,
                 message: 'Error Occured Contact Admin'
             })
-            
+            return 
         })
-    })
+    }
+    
 }
