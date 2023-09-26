e5b6b1e8692d634cf0f877d2fef03a3edbeba8ab
Avei20 <muhammadabdulazizalghofari@gmail.com>
2023-09-26 08:36:39 +0700

    updating codes

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,6 @@
 import process from "process";
 
-export const JWT_KEY = process.env.JWT_KEY || 'oeatunoehsuaotnheu'
-export const SERVER_PORT = process.env.PORT || '8080'
-export const TOKEN_VALID_TIME = process.env.TOKEN_VALID_TIME || '48h'
-export const ROOT_URL = process.env.ROOT_URL || 'localhost:3000'
+export const JWT_KEY = process.env.JWT_KEY
+export const SERVER_PORT = process.env.PORT
+export const TOKEN_VALID_TIME = process.env.TOKEN_VALID_TIME 
+export const ROOT_URL = process.env.ROOT_URL 
